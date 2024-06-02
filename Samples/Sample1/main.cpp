eaf2e4bb38486779c6dade768accbdac5a81b1c9
Balalarga <lightman03@mail.ru>
2024-06-02 18:31:06 +0300

    Add gtest, filesystem creation for virtual paths

--- Samples/Sample1/main.cpp
+++ Samples/Sample1/main.cpp
@@ -1,30 +1,7 @@
 #include <iostream>
-#include "GlfwWindow/GlfwWindow.h"
-#include "UserInput/InputManager.h"
-#include "nlohmann/json.hpp"
-
-using namespace Inf;
 
 
-int main(int, char**)
-{
-	WindowParams params;
-	params.fps = 60;
-
-	const std::unique_ptr<GlfwWindow> window = std::make_unique<GlfwWindow>(params);
-
-	KeyBindHandle handle(
-		KeyboardButtons::W,
-		[](float val, const std::set<KeyboardModifiers>& mods)
-		{
-			if (val > 0)
-				std::cout << "Pressed\n";
-			else
-				std::cout << "Released\n";
-		});
-
-	window->Run();
-
-
-	return 0;
+int main(int, char **) {
+    std::cout << "main()\n";
+    return 0;
 }
